7d4cf01ca3e70b9926253c831bb8bd40c9d3b2dd
LiamCurran <LiamCurran95>
2022-02-28 14:33:15 +0000

    Adding footer and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+	return (
+		<div>
+			<h1>Meteorite Mapping Website</h1>
+			<h2>To use the app:</h2>
+			<div className="instructions">
+				<p>Insert generic text</p>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
