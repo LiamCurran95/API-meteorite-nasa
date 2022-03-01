692026d4d5d8878910b209960dd7998b0abf9e95
Tom wallwin <tomwallwin@toms-mbp.home>
2022-03-01 15:33:02 +0000

    map and table working no filter

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,19 +3,27 @@ import DataTable from "./DataTable.jsx";
 import { useEffect, useState } from "react";
 import fetchMeteors from "../utils/api.js";
 export default function Body() {
-	const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-	useEffect(() => {
-		fetchMeteors().then((fetchedData) => {
-			setData(fetchedData);
-		});
-	}, []);
+  useEffect(() => {
+    fetchMeteors().then((fetchedData) => {
+      fetchedData = fetchedData.filter((meteor) => meteor.year);
+      setData(
+        fetchedData.map((meteor) => {
+          meteor.year = meteor.year.slice(0, 4);
+          return meteor;
+        })
+      );
+    });
+  }, []);
 
-	return (
-		<>
-			<DataTable data={data} />
-			<br></br>
-			<Map data={data} />
-		</>
-	);
+  return (
+    <>
+      <DataTable data={data} />
+      <br></br>
+      <button>Filter Between Years:</button>
+      <input type="text"></input>
+      <Map data={data} />
+    </>
+  );
 }
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,42 +1,41 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { icon } from "leaflet";
-import { useEffect, useState } from "react";
+
+import { useState } from "react";
 
 export default function Map(props) {
-	const meteors = props.data.filter((meteor) => meteor.reclat != undefined);
-	const [activeMeteorite, setActiveMeteorite] = useState(null);
+  const meteors = props.data.filter((meteor) => meteor.reclat != undefined);
+  const [activeMeteorite, setActiveMeteorite] = useState(null);
 
-	return (
-		<div id="map">
-			<MapContainer center={[51.505, -0.09]} zoom={1}>
-				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-				/>
-				{meteors.map((meteorite) => {
-					return (
-						<Marker
-							key={meteorite.id}
-							position={[+meteorite.reclat, +meteorite.reclong]}
-							onClick={() => {
-								setActiveMeteorite(meteorite);
-							}}
-						/>
-					);
-				})}
-				{activeMeteorite && (
-					<Popup
-						position={[
-							activeMeteorite.geometry.coordinates[0],
-							activeMeteorite.geometry.coordinates[1],
-						]}
-					>
-						<div>
-							<h2>{activeMeteorite.properties.name}</h2>
-						</div>
-					</Popup>
-				)}
-			</MapContainer>
-		</div>
-	);
+  return (
+    <div id="map">
+      <MapContainer center={[51.505, -0.09]} zoom={1}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {meteors.map((meteorite) => {
+          return (
+            <Marker
+              key={meteorite.id}
+              position={[+meteorite.reclat, +meteorite.reclong]}
+              eventHandlers={{
+                click: () => {
+                  setActiveMeteorite(meteorite);
+                }
+              }}
+            />
+          );
+        })}
+        {activeMeteorite && (
+          <Popup position={[+activeMeteorite.reclat, +activeMeteorite.reclong]}>
+            <div>
+              <h2>{activeMeteorite.name}</h2>
+              <h3>{activeMeteorite.year}</h3>
+              <h3>{activeMeteorite.mass}g</h3>
+            </div>
+          </Popup>
+        )}
+      </MapContainer>
+    </div>
+  );
 }
