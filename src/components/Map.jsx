2f3d9c424f540486d63bd97a85dac42f21e0624d
LiamCurran <LiamCurran95>
2022-03-01 12:46:37 +0000

    Adding map and updating fetch request

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,1 +1,25 @@
-export default function Map() {}
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { icon } from "leaflet";
+
+export default function Map(props) {
+	const meteors = props.data.filter((meteor) => meteor.reclat != undefined);
+
+	return (
+		<div id="map">
+			<MapContainer center={[51.505, -0.09]} zoom={1}>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				{meteors.map((meteorite) => {
+					return (
+						<Marker
+							key={meteorite.id}
+							position={[+meteorite.reclat, +meteorite.reclong]}
+						/>
+					);
+				})}
+			</MapContainer>
+		</div>
+	);
+}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,13 @@
-export default function fetchFromApi() {
-  return fetch("https://data.nasa.gov/resource/gh4g-9sfh.json")
-    .then((data) => {
-      return data.json();
-    })
-    .then((data) => {
-      return data;
-    })
-    .catch(console.log);
+export default function fetchMeteors() {
+	return fetch(
+		"https://data.nasa.gov/api/id/gh4g-9sfh.json?$select=`name`,`id`,`nametype`,`recclass`,`mass`,`year`,`reclat`,`reclong`&$order=`:id`+ASC&$limit=50000&$offset=0"
+	)
+		.then((data) => {
+			return data.json();
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
+
+//original API https://data.nasa.gov/resource/gh4g-9sfh.json
